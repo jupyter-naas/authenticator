7e184b7fcd600dc82fd4b1b1d8b3d5602f65696a
GitHub Action <action@github.com>
2021-04-22 17:55:25 +0000

    bump: version 0.3.75 → 0.3.76

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="jupyterhub-naasauthenticator",
-    version="0.3.75",
+    version="0.3.76",
     description="JupyterHub Native Authenticator",
     long_description=long_description,
     long_description_content_type="text/markdown",
